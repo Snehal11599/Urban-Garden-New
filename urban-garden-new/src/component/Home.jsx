284c630772916583cffe7c46a2eeedc2f1d92704
khushaliargade <95602831+khushaliargade@users.noreply.github.com>
2022-03-07 12:38:25 +0530

    Routing

---
+++ urban-garden-new/src/component/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Img from './Img'
+
+export default function Home() {
+    return (
+        <div>
+            <>
+                <Img />
+            </>
+        </div>
+    )
+}
