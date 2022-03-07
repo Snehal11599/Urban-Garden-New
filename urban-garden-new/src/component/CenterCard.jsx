3b97b47b74ed31c813863f5bb8c3173f69c91e24
Snehal11599 <snehalc11599@gmail.com>
2022-03-07 12:35:25 +0530

    home

---
+++ urban-garden-new/src/component/CenterCard.jsx
@@ -0,0 +1,56 @@
+import React from 'react'
+
+
+
+
+const CenterCard = () => {
+    return (
+        <div className='container'>
+            <h1>Why Choose Us?</h1>
+            <img src="/assets1/HomeImages/icon.png" alt='..' />
+            <h3>Every case is very important to us and we always take care of them seriously.</h3>
+            <div className="row row-cols-1 row-cols-md-3 g-4 " style={{ margin: "75px" }}>
+                <div className="col">
+                    <div className="card">
+                        <img src="..." className="card-img-top" alt="..." />
+                        <div className="card-body">
+                            <h5 className="card-title">Affordabel Pricing</h5>
+                            <p className="card-text">Dolor sit amet dolor gravida placerat liberolorem ipsum dolor consectetur adipiscing elit, sed do eiusmod.</p>
+                        </div>
+                    </div>
+                </div>
+                <div className="col">
+                    <div className="card">
+                        <img src="..." className="card-img-top" alt="..." />
+                        <div className="card-body">
+                            <h5 className="card-title">The Best in Pekanbaru</h5>
+                            <p className="card-text">Dolor sit amet dolor gravida placerat liberolorem ipsum dolor consectetur adipiscing elit, sed do eiusmod.</p>
+                        </div>
+                    </div>
+                </div>
+                <div className="col">
+                    <div className="card">
+                        <img src="..." className="card-img-top" alt="..." />
+                        <div className="card-body">
+                            <h5 className="card-title">Certified Expert</h5>
+                            <p className="card-text">Dolor sit amet dolor gravida placerat liberolorem ipsum dolor consectetur adipiscing elit, sed do eiusmod.</p>
+                        </div>
+                    </div>
+                </div>
+                <div className="col">
+                    <div className="card">
+                        <img src="..." className="card-img-top" alt="..." />
+                        <div className="card-body">
+                            <h5 className="card-title">High Quality Services</h5>
+                            <p className="card-text">Dolor sit amet dolor gravida placerat liberolorem ipsum dolor consectetur adipiscing elit, sed do eiusmod.</p>
+                        </div>
+                    </div>
+
+                </div>
+
+            </div>
+        </div>
+    )
+}
+
+export default CenterCard
---
+++ urban-garden-new/src/component/Home.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Carousel from './Carousel'
+import CenterCard from './CenterCard'
+
+
+const Home = () => {
+    return (
+        <div>
+            <Carousel />
+            <CenterCard />
+        </div>
+    )
+}
+
+export default Home
