d001017ee7a7a111b7b778ce6884f206f2d2a868
Snehal11599 <snehalc11599@gmail.com>
2022-03-08 15:32:56 +0530

    form

--- urban-garden-new/src/component/ContactForm.jsx
+++ urban-garden-new/src/component/ContactForm.jsx
@@ -19,23 +19,23 @@ const ContactForm = () => {
                 <form className="row g-3">
                     <div className="col-md-6">
                         <label for="inputEmail4" className="form-label">Email</label>
-                        <input type="email" className="form-control" id="inputEmail4"/>
+                        <input type="email" className="form-control" id="inputEmail4" />
                     </div>
                     <div className="col-md-6">
                         <label for="inputPassword4" className="form-label">Password</label>
-                        <input type="password" className="form-control" id="inputPassword4"/>
+                        <input type="password" className="form-control" id="inputPassword4" />
                     </div>
                     <div className="col-12">
                         <label for="inputAddress" className="form-label">Address</label>
-                        <input type="text" className="form-control" id="inputAddress" placeholder="1234 Main St"/>
+                        <input type="text" className="form-control" id="inputAddress" placeholder="1234 Main St" />
                     </div>
                     <div className="col-12">
                         <label for="inputAddress2" className="form-label">Address 2</label>
-                        <input type="text" className="form-control" id="inputAddress2" placeholder="Apartment, studio, or floor"/>
+                        <input type="text" className="form-control" id="inputAddress2" placeholder="Apartment, studio, or floor" />
                     </div>
                     <div className="col-md-6">
                         <label for="inputCity" className="form-label">City</label>
-                        <input type="text" className="form-control" id="inputCity"/>
+                        <input type="text" className="form-control" id="inputCity" />
                     </div>
                     <div className="col-md-4">
                         <label for="inputState" className="form-label">State</label>
@@ -46,7 +46,7 @@ const ContactForm = () => {
                     </div>
                     <div className="col-md-2">
                         <label for="inputZip" className="form-label">Zip</label>
-                        <input type="text" className="form-control" id="inputZip"/>
+                        <input type="text" className="form-control" id="inputZip" />
                     </div>
                     <div className="col-12">
                         <div className="form-check">
