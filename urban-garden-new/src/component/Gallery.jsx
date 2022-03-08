0a46d10303638b28a46826819a810df3a12a8df3
khushaliargade <95602831+khushaliargade@users.noreply.github.com>
2022-03-08 20:05:30 +0530

    Testimonials

--- urban-garden-new/src/component/Gallery.jsx
+++ urban-garden-new/src/component/Gallery.jsx
@@ -7,6 +7,9 @@ const Gallery = () => {
     <div>
         <Banner/>
         <CenterImages/>
+        
+        
+        
     </div>
   )
 }
--- urban-garden-new/src/component/Testimonials.jsx
+++ urban-garden-new/src/component/Testimonials.jsx
@@ -1,9 +1,86 @@
 import React from 'react'
+import Banner from './Banner'
 
-const Testimonials = () => {
+const Card = () => {
   return (
-    <div>Testimonials</div>
+
+    <div>
+      <Banner />
+      <div className="card mb-3" style={{ width: "540px" }}>
+        <div className="row g-0">
+          <div className="col-md-4">
+            <img src="/assets/Testimonials/team-img1.jpg" className="img-fluid rounded-start" alt="..." />
+          </div>
+          <div className="col-md-8">
+            <div className="card-body">
+              <h3 className="card-title">-JOHN DOEL</h3>
+              <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+              <p className="card-text"><small className="text-muted">Last updated 3 mins ago</small></p>
+            </div>
+          </div>
+        </div>
+      </div>
+
+
+      <div className="card mb-3" style={{ width: "540px" }}>
+        <div className="row g-0">
+          <div className="col-md-4">
+            <img src="/assets/Testimonials/team-img2.jpg" className="img-fluid rounded-start" alt="..." />
+          </div>
+          <div className="col-md-8">
+            <div className="card-body">
+              <h3 className="card-title">-RAISA DOEL</h3>
+              <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+              <p className="card-text"><small className="text-muted">Last updated 3 mins ago</small></p>
+            </div>
+          </div>
+        </div>
+      </div>
+
+
+      <div className="card mb-3" style={{ width: "540px" }}>
+        <div className="row g-0">
+          <div className="col-md-4">
+            <img src="/assets/Testimonials/team-img3.jpg" className="img-fluid rounded-start" alt="..." />
+          </div>
+          <div className="col-md-8">
+            <div className="card-body">
+              <h3 className="card-title">-JOSH DOEL</h3>
+              <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+              <p className="card-text"><small className="text-muted">Last updated 3 mins ago</small></p>
+            </div>
+          </div>
+        </div>
+      </div>
+
+
+      <div className="card mb-3" style={{ width: "540px" }}>
+        <div className="row g-0">
+          <div className="col-md-4">
+            <img src="/assets/Testimonials/team-img4.jpg" className="img-fluid rounded-start" alt="..." />
+          </div>
+          <div className="col-md-8">
+            <div className="card-body">
+              <h3 className="card-title">-SASHA DOEL</h3>
+              <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+              <p className="card-text"><small className="text-muted">Last updated 3 mins ago</small></p>
+            </div>
+          </div>
+        </div>
+      </div>
+
+
+
+
+
+
+
+    </div>
+
+
+
+
   )
 }
 
-export default Testimonials
+export default Card
