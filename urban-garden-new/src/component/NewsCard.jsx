8c1aac24061435b898914fd98d285af8c8a5d28a
khushaliargade <95602831+khushaliargade@users.noreply.github.com>
2022-03-09 20:27:14 +0530

    update-9/3/2022
    json server

--- urban-garden-new/src/component/NewsCard.jsx
+++ urban-garden-new/src/component/NewsCard.jsx
@@ -1,35 +1,31 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 
 const NewsCard = () => {
+    const [posts, setPosts] = useState([])
+
+
+
+
+    useEffect(() => {
+        const url = 'http://localhost:3000/articles';
+        fetch(url).then(resp => resp.json())
+            .then(resp => setPosts(resp))
+
+    }, [])
     return (
         <div>
-            <div className="card" style={{ width: "70rem" }}>
-                <img src="/assets/News/news-img-1.jpg" className="card-img-top" alt="..." />
-                <div className="card-body">
-                    <h3 className="card-title">Why You Have Difficult To Clean Your Lawn</h3>
-                    <p className="card-text">We provide high quality design at vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores...</p>
-                    <a href="#" className="btn btn-primary">Go somewhere</a>
-                </div>
-            </div><br></br>
-
-            <div className="card" style={{ width: "70rem" }}>
-                <img src="/assets/News/news-img-2.jpg" className="card-img-top" alt="..." />
-                <div className="card-body">
-                    <h3 className="card-title">We Open Recruitment For LandScaping</h3>
-                    <p className="card-text">We provide high quality design at vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores...</p>
-                    <a href="#" className="btn btn-primary">Go somewhere</a>
-                </div>
-            </div>
-            <br></br>
-
-            <div className="card" style={{ width: "70rem" }}>
-                <img src="/assets/News/news-img-3.jpg" className="card-img-top" alt="..." />
-                <div className="card-body">
-                    <h3 className="card-title">We Offer 30% Cashback For Flower Garden</h3>
-                    <p className="card-text">We provide high quality design at vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores...</p>
-                    <a to="/" className="btn btn-primary">Go somewhere</a>
-                </div>
+            <div className="card" style={{ width: "50rem" }}>
+                {
+                    posts.map(post => {
+                        return <div>
+                            <img src={post.urlToImage} className="card-img-top" alt="..." />
+                            <h3 className="card-title">{post.title}</h3>
+                            <p className="card-text">{post.description}</p>
+                            <a href="#" className="btn btn-primary">Go somewhere</a>
+                        </div>
+                    }
+                    )}
             </div><br></br>
             <div className="'col-xs-3'" style={{ margin: "left:30 px" }}>
                 <button type="button" className="btn btn-primary btn-lg" style={{ marginRight: "20px" }}>Previous</button>
